e65da3181112a823bdd07bd4d7a1e910d143f10f
Sam <samhooker@gmail.com>
2024-10-09 21:27:28 -0700

    footer images

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,7 +1,7 @@
 <section id="footer" class="w-full bg-veryDarkBlue py-6 sm:py-8">
   <div class="container mx-auto flex flex-col gap-4 md:flex-row md:justify-between items-center">
     <div class="flex flex-col md:flex-row md:justify-start items-center gap-10 sm:gap-4">
-      <img src="src/lib/images/logo-dark.svg" alt="logo" class="fill-cyan-500 dark-logo md:mt-3 md:mr-10">
+      <img src="https://raw.githubusercontent.com/35degrees/bookmark-landing-fem/refs/heads/main/src/lib/images/logo-dark.svg" alt="logo" class="fill-cyan-500 dark-logo md:mt-3 md:mr-10">
       <ul class="flex flex-col md:flex-row gap-10 items-center justify-center sm:gap-4 sm:mt-3">
         <li class="text-xs opacity-60 text-white hover:opacity-90 uppercase"><a href="#" class="text-widest">Features</a></li>
         <li class="text-xs opacity-60 text-white hover:opacity-90 uppercase"><a href="#" class="text-widest">Pricing</a></li>
@@ -9,8 +9,8 @@
       </ul>
     </div>
     <div class="flex flex-row gap-10 items-center justify-center sm:mt-5">
-      <img src="src/lib/images/icon-facebook.svg" alt="fb">
-      <img src="src/lib/images/icon-twitter.svg" alt="tw">
+      <img src="https://raw.githubusercontent.com/35degrees/bookmark-landing-fem/refs/heads/main/src/lib/images/icon-facebook.svg" alt="fb">
+      <img src="https://raw.githubusercontent.com/35degrees/bookmark-landing-fem/refs/heads/main/src/lib/images/icon-twitter.svg" alt="tw">
     </div>
   </div>
 </section>
